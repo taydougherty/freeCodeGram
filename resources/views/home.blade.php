f5c47973a7d9a0a555f24e7cef7acc4f7fbaeff2
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-08-24 18:23:52 -0700

    added content and styling to heading/bio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://cdn.changelog.com/uploads/icons/news_sources/Qo/icon_large.png?v=63680075631" class="rounded-circle" style="height: 125px;">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>freecodecamp</h1>
             </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freecodecamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
         </div>
     </div>
 </div>
