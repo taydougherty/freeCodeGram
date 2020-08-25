2093c11ce1b77378aeaf4d1537c28e08562ee94f
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-08-25 08:26:55 -0700

    added profile ui and static pictures

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,5 +20,16 @@
             <div><a href="#">www.freecodecamp.org</a></div>
         </div>
     </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://scontent-lax3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.12.775.775a/s640x640/117904071_586448102051174_5646477369680450633_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com&_nc_cat=102&_nc_ohc=v7fr3uwTmkgAX8Dko_C&oh=7618df7625b9a5a5e9d03819bfb7a6cc&oe=5F6F429B" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-lax3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c1.0.748.748a/s640x640/117532617_329372988207182_2163166929089500044_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com&_nc_cat=102&_nc_ohc=SbJMjBlKPXgAX86E2M7&oh=a02236ff4457a048dfc159d3b4edba97&oe=5F6D2400" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-lax3-2.cdninstagram.com/v/t51.2885-15/e35/c96.0.558.558a/117548414_624348341845326_8249234937592266135_n.jpg?_nc_ht=scontent-lax3-2.cdninstagram.com&_nc_cat=106&_nc_ohc=RvekRIJ0cNUAX-8umiK&oh=1dcb0ce2cb9556cee537400f6e84d19e&oe=5F6DDD96" class="w-100">
+        </div>
+    </div>
 </div>
 @endsection
