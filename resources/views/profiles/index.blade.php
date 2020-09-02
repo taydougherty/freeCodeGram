a7ce1b385c05baf7dd12f805b1f2a14c4665cdf4
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-09-02 11:20:48 -0700

    added route model binding

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -24,8 +24,10 @@
     <div class="row pt-5">
         @foreach($user->posts as $post)
 
-        <div class="col-4">
-            <img src="/storage/{{$post->image}}" class="w-100">
+        <div class="col-4 pb-4">
+            <a href="/p/{{$post->id}}">
+                <img src="/storage/{{$post->image}}" class="w-100">
+            </a>
         </div>
         @endforeach
     </div>
