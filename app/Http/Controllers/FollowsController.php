823fc2d9e557abbaa17b11d0f850da2ffd4a23ca
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-09-03 16:43:18 -0700

    added follow button vue component and routes

---
+++ app/Http/Controllers/FollowsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class FollowsController extends Controller
+{
+    public function store(User $user){
+        return $user->username;
+    }
+}
