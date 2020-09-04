be87978401f5937404ca639b62b1fe4a2e0e5bca
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-09-03 19:41:22 -0700

    added mailtrap and updated register route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Mail\NewUserWelcomeMail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Auth::routes();
+
+Route::get('/email', function(){
+    return new NewUserWelcomeMail();
+});
 
 Route::post('follow/{user}', 'FollowsController@store');
 
