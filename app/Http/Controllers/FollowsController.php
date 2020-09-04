1b01c26f74d34f30eb7b3ca7b64a92c786ff7f96
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-09-03 18:13:45 -0700

    added functionality to follow button and redirect unauthorized users

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -7,7 +7,12 @@ use App\User;
 
 class FollowsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function store(User $user){
-        return $user->username;
+        return auth()->user()->following()->toggle($user->profile);
     }
 }
